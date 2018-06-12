852e85d637a8dbbf0a67bd08f9c617050f3ffbd2
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-06-12 16:43:51 +0300

    Синглтон

--- src/Application.php
+++ src/Application.php
@@ -2,25 +2,69 @@
 namespace ItForFree\SimpleMVC;
 
 use ItForFree\rusphp\PHP\ArrayLib\DotNotation\Dot;
+use ItForFree\SimpleMVC\exceptions\SmvcCoreException;
 
 /**
  * Класс-"точка входа" для работы фреймворка SimpleMVC
+ * 
+ * Реализует паттерн "Одиночка" @see http://fkn.ktu10.com/?q=node/5572
  */
 class Application
 {
     
+    /**
+     * Массив конфигурации приложенияъ
+     * 
+     * @var ItForFree\rusphp\PHP\ArrayLib\DotNotation\Dot
+     */
+    public $config = null;
     
     
-    public function __construct($config) {
-        ;
+    /**
+     * Cкрываем конструктор для того чтобы класс нельзя было создать в обход get() 
+     */
+    protected function __construct() {}
+    
+    /**
+     * Метод для получения текущего объекта приложения
+     * 
+     * @staticvar type $instance
+     * @return ItForFree\SimpleMVC\Applicaion объект приложения
+     */
+    public static function get()
+    {
+        static $instance = null; // статическая переменная
+        if (null === $instance) { // проверка существования
+            $instance = new static();
+        }
+        return $instance;
     }
+    
+
     /**
      * Запускает приложение
      * 
      * @param array $config Конфигурационный масиив приложения
      */
-    public function run($config) {
-        $route = \core\mvc\view\Url::getRoute();
-        $obj = new \core\Router($route);
+    public function run() {
+        
+        if (!empty($this->config)) {
+            
+            $route = \core\mvc\view\Url::getRoute();
+            $obj = new \core\Router($route);
+            
+        } else {
+            throw new SmvcCoreException ('Не задан конфигурационный массив приложения!');
+        }
+
+        
+        return $this;
+    }
+    
+    
+    public function setConfiguration($config)
+    {
+        $this->config = new Dot($config);
+        return $this;
     }
 }
