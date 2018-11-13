89dd19e8ddcf1f6df4a0b0bcb6678c5768f60506
vedro-compota <ru-ibn-hottab@yandex.ru>
2018-11-13 18:51:00 +0300

    Продолжаем разработку минималистичного управления asset-ами

--- src/components/SimpleAsset/SimpleAsset.php
+++ src/components/SimpleAsset/SimpleAsset.php
@@ -3,12 +3,19 @@
 namespace ItForFree\SimpleMVC\components\SimpleAsset;
 
 use ItForFree\SimpleMVC\components\SimpleAsset\SimpleAssetManager;
+use ItForFree\rusphp\File\Path;
+use ItForFree\rusphp\PHP\Hash\LengthHash;
+use ItForFree\rusphp\Common\Directory\Directory;
 
 /**
  * Базовый класс
  */
-class SimpleAsset extends SimpleAssetManager
+class SimpleAsset
 {
+   /**
+    * Путь относительно корня сайта к базовой директории
+    * @var string 
+    */
    public $basePath = '';
    
    public $js = [];
@@ -20,10 +27,69 @@ class SimpleAsset extends SimpleAssetManager
     * далее можно будет распечатать в шаблоне с помощью
     * класса SimpleAssetManager
     */
-   public function add()
+   public static function add()
    {
-       SimpleAssetManager::addAsset($this);
+       $Asset = new static();
+       $Asset->basePath = Path::addToDocumentRoot($Asset->basePath); // делаем относительный путь абсолютным
+       SimpleAssetManager::addAsset($Asset);
+   }
+
+  
+   protected function publish()
+   {
+       $baseAssetPublishPath = $this->basePath . LengthHash::md5($this::class, 10);
+       
+       if (!is_dir($baseAssetPublishPath)) { // создаём базовую директорию, если её нет
+            if(!mkdir($baseAssetPublishPath, 0777, true)) {
+                throw new \Exception("Не удалось создать директорию $baseAssetPublishPath");
+            }
+       }
+       
+       $lastChangeFileTimestamp = $this->getLastChangeFileTimestamp();
+       $baseAssetTimePath = $baseAssetPublishPath 
+            . DIRECTORY_SEPARATOR . $lastChangeFileTimestamp;
+       
+       if (!is_dir($baseAssetTimePath)) {
+           return; // если ничего не изменилось
+       } else {
+           Directory::clear($baseAssetPublishPath); // полностью очищаем родительскую директорию
+           $this->copyToAssetsDir($baseAssetTimePath . DIRECTORY_SEPARATOR);
+       }
    }
    
-   
+   protected function copyToAssetsDir($basePublishPath)
+   {
+        $assetSourcePath = $this->basePath;
+        foreach ($this->js as $filePath) {
+            $fullPubPath = $basePublishPath . 'js/' 
+                    . DIRECTORY_SEPARATOR . $filePath;
+            $fullSourcePath = $assetSourcePath . DIRECTORY_SEPARATOR . $filePath;
+            copy($fullSourcePath, $fullPubPath); 
+        }
+        
+        foreach ($this->css as $filePath) {
+            $fullPubPath = $basePublishPath . 'css/' 
+                    . DIRECTORY_SEPARATOR . $filePath;
+            $fullSourcePath = $assetSourcePath . DIRECTORY_SEPARATOR . $filePath;
+            copy($fullSourcePath, $fullPubPath); 
+        }
+   }
+
+   protected function getLastChangeFileTimestamp()
+   {
+        $assetSourcePath = $this->basePath;
+        $lasttime = null;
+
+        foreach ($this->js as $filePath) {
+            $fullPath = $assetPubPath . DIRECTORY_SEPARATOR . $filePath;
+            $lasttime = filemtime($fullPath);
+        }
+        
+        foreach ($this->css as $filePath) {
+            $fullPath = $assetPubPath . $filePath;
+            $lasttime = filemtime($fullPath);
+        }
+        
+        return $lasttime; 
+   } 
 }
--- src/components/SimpleAsset/SimpleAssetManager.php
+++ src/components/SimpleAsset/SimpleAssetManager.php
@@ -7,23 +7,57 @@ namespace ItForFree\SimpleMVC\components\SimpleAsset;
  */
 class SimpleAssetManager
 {
-    protected $jsList = [];
+    public static $assetsPath = 'assets/';
     
-    protected $cssList = [];
+    /**
+     * Данные об используемых js файлвх
+     * @var array 
+     */
+    protected static $jsList = [];
+    
+    /**
+     * Данные об используемых CSS файлах
+     * @var array
+     */
+    protected static $cssList = [];
+    
+    /**
+     * Массив объектов-ассетов, которые нужно будет использовать на странице 
+     * (на которых был вызван метод ->add())
+     * 
+     * @var array объектов ItForFree\SimpleMVC\components\SimpleAsset\SimpleAsset
+     */
+    protected static $assets = [];
     
     public static function printJs()
     {
+        static::publishJs();
+        
         foreach (static::$jsList as $js) {
             echo("<script type=\"text/javascript\" src=\"$js\"></script>\n");
         }
     }
     
     /**
-     * Добавит ассет в глобальный список
+     * Добавит ассет в глобальный список (зарегистрирует его)
      * 
-     * @param type $SimpleAssetObject
+     * @param \ItForFree\SimpleMVC\components\SimpleAsset\SimpleAsset $SimpleAssetObject
      */
     public static function addAsset($SimpleAssetObject)
+    {
+        static::$assets[] = $SimpleAssetObject;
+        
+        foreach ($SimpleAssetObject->js as $jsFile) {
+            static::$jsList[] = [
+                'source' => $jsFile,
+                'assetClassName' => $SimpleAssetObject::class,
+
+            ];
+        }
+        $SimpleAssetObject->publish();
+    }
+
+    protected static function publishJs()
     {
         
     }
