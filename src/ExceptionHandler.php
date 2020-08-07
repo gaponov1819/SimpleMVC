d54a18c46a2d467365efe3c51566409dd1c72e09
Михаил <toyrik@gmail.com>
2020-08-07 16:19:30 +0300

    Рефакторим метод обработки ошибок
    В классе ротера в методе вызова действия контроллера добавлена проверка
    на наличие файла.

--- src/ExceptionHandler.php
+++ src/ExceptionHandler.php
@@ -11,18 +11,25 @@ class ExceptionHandler implements ExceptionHandlerInterface
     {
         set_exception_handler(array($this, 'handleException'));
     }
-
+    
+    /**
+     * Метод обработки исключения. Проверяет существует ли пользовательский обработчик.
+     * Если пользовательский обработчик найден, то исключение передаётся ему на обработку.
+     * Если пользовательского обработчика нет то исключение никак не обрабатывается.
+     * @param \Exception $exception
+     * @return void
+     * @throws \Exception
+     */
     public function handleException(\Exception $exception): void
     {
         $handlers = Config::get('core.handlers');
-        foreach ($handlers as $exceptionName => $handlerName){
-            if ($exceptionName == get_class($exception)){
-                $thatHandler = Config::getObject('core.handlers.'.$exceptionName);
-                $thatHandler->handleException($exception);
-            }else{
-                throw $exception;
-            }
+        
+        if(array_key_exists(get_class($exception), $handlers)){
+            $exceptionName = get_class($exception);
+            $thatHandler = Config::getObject('core.handlers.'.$exceptionName);
+            $thatHandler->handleException($exception);
+        } else {
+            throw $exception;
         }
     }
-
 }
